bbb6ed2f922dca7ce766a783a81341dad2b3142b
stickell <l.stickell@yahoo.it>
2015-02-11 18:07:46 +0100

    [GIT] Resetting the working copy just before pulling
    Sometimes the repo on the server is ahead of origin (???). Resetting to avoid this

--- lib/git.inc.php
+++ lib/git.inc.php
@@ -32,7 +32,7 @@ class GitCMD {
     }
 
     public function pull() {
-        $output = shell_exec("cd $this->git_path; git pull -s recursive -X theirs");
+        $output = shell_exec("cd $this->git_path; git reset --hard origin/$this->branch; git pull -s recursive -X theirs");
         if(is_null($output)) {
             $this->l->error('An error occurred fetching the git repo. Exiting.');
             exit("An error occurred fetching the git repo. Exiting.\n");
